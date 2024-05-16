8443c0946a6824d0b72c71779f2f21c88076618d
Alex Nepsha <nepsha1986@gmail.com>
2024-05-16 14:36:39 +0300

    feature. base settings and initialization

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# chart
+# Canvas Chart - Forextester Assigment for Middle+/Senior Frontend Developer Position.
+
+## Assignment Notes:
+
+Creates project with [Vite](https://vitejs.dev/)
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './style.css'
+import { App } from './App.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<div id="counter" />`
+
+App(document.querySelector<HTMLButtonElement>('#counter')!)
